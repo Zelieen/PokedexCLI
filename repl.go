ae3585e80f41c39eebbf56ebaa24e590ee0d4701
Zelieen <firina@gmx.net>
2025-04-15 15:41:54 +0200

    feat: add basic interactivity

--- repl.go
+++ repl.go
@@ -1,8 +1,26 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"os"
 	"strings"
 )
+
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		input := scanner.Text()
+		if len(input) == 0 {
+			continue
+		}
+		firstWord := cleanInput(input)[0]
+		fmt.Printf("Your command was: %s\n", firstWord)
+	}
+}
 
 func cleanInput(text string) []string {
 	var cleanedInput []string
