df32202865f308d6d5e3d5c379c6390cd1711d2a
Zelieen <firina@gmx.net>
2025-04-18 23:47:40 +0200

    feat: get basic response body for location-area

--- internal/pokeAPI/pokeAPI.go
+++ internal/pokeAPI/pokeAPI.go
@@ -1,7 +1,41 @@
 package pokeAPI
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"net/http"
+	//"encoding/json"
+)
 
-func PokeTest() {
-	fmt.Println("But PokeAPI says hi.")
+type LocationArea struct {
+	Count    int    `json:"count"`
+	Next     string `json:"next"`
+	Previous string `json:"previous"`
+	Results  []struct {
+		Name string `json:"name"`
+		URL  string `json:"url"`
+	} `json:"results"`
+}
+
+func PokeLocationArea() {
+	fmt.Println("But PokeAPI function says hi.")
+
+	//area := LocationArea{}
+
+	res, err := http.Get("https://pokeapi.co/api/v2/location-area/?limit=10&offset=0")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	body, err := io.ReadAll(res.Body)
+	res.Body.Close()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	if res.StatusCode > 299 {
+		fmt.Printf("Response failed. Status code is %d\nThe response body:\n%s", res.StatusCode, body)
+		return
+	}
+	fmt.Printf("The response body:\n%s\n", body)
 }
